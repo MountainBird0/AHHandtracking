79a222cd343d70fba7417c13dbeaec47149f3568
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2022-11-02 13:55:18 +0900

    [11.02] add hand ui
    터치 ui 추가

---
+++ Assets/00.Practice/Scripts/InputCheck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCheck : MonoBehaviour
+{
+    public void ClickButton()
+    {
+        Debug.Log("버튼 누름");
+    }
+}
